92f109d05f41840829bf0dde6eb3336a024ee36a
Kartavya Patel <kartavyapatel630@gmail.com>
2026-06-30 16:26:41 +0530

    Making the main structure

---
+++ Visualizer.py
@@ -0,0 +1,159 @@
+import pandas as pd
+import numpy as np
+from abc import ABC, abstractmethod
+
+
+class sales(ABC):
+    @abstractmethod
+    def __init__(self):
+        pass
+
+    @abstractmethod
+    def load_data(self):
+        pass
+
+    @abstractmethod
+    def explore_data(self):
+        pass
+
+    @abstractmethod
+    def clean_data(self):
+        pass
+
+    @abstractmethod
+    def mathematical_operations(self):
+        pass
+
+    @abstractmethod
+    def combine_data(self):
+        pass
+
+    @abstractmethod
+    def split_data(self):
+        pass
+
+    @abstractmethod
+    def search_sort_filter(self):
+        pass
+
+    @abstractmethod
+    def aggregate_functions(self):
+        pass
+
+    @abstractmethod
+    def statistical_analysis(self):
+        pass
+
+    @abstractmethod
+    def create_pivot_table(self):
+        pass
+
+    @abstractmethod
+    def visualize_data(self):
+        pass
+
+    @abstractmethod
+    def __init__(self):
+        pass
+
+    @abstractmethod
+    def load_data(self):
+        pass
+
+    @abstractmethod
+    def explore_data(self):
+        pass
+
+    @abstractmethod
+    def clean_data(self):
+        pass
+
+    @abstractmethod
+    def mathematical_operations(self):
+        pass
+
+    @abstractmethod
+    def combine_data(self):
+        pass
+
+    @abstractmethod
+    def split_data(self):
+        pass
+
+    @abstractmethod
+    def search_sort_filter(self):
+        pass
+
+    @abstractmethod
+    def aggregate_functions(self):
+        pass
+
+    @abstractmethod
+    def statistical_analysis(self):
+        pass
+
+    @abstractmethod
+    def create_pivot_table(self):
+        pass
+
+    @abstractmethod
+    def visualize_data(self):
+        pass
+
+    @abstractmethod
+    def __del__(self):
+        pass
+
+
+class SalesDataAnalzer(sales):
+
+    def __init__(self):
+        pass
+
+    def load_data(self):
+        pass
+
+    def explore_data(self):
+        pass
+
+    def clean_data(self):
+        pass
+
+    def mathematical_operations(self):
+        pass
+
+    def combine_data(self):
+        pass
+
+    def split_data(self):
+        pass
+
+    def search_sort_filter(self):
+        pass
+
+    def aggregate_functions(self):
+        pass
+
+    def statistical_analysis(self):
+        pass
+
+    def create_pivot_table(self):
+        pass
+
+    def visualize_data(self):
+        pass
+
+    def __del__(self):
+        pass
+
+
+def show_menu():
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
